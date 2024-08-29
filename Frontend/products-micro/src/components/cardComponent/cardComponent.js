b34b3f3651c6184d3832cb46030a996d16089502
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-08-30 01:21:49 +0300

    edit on card component

--- Frontend/products-micro/src/components/cardComponent/cardComponent.js
+++ Frontend/products-micro/src/components/cardComponent/cardComponent.js
@@ -2,32 +2,31 @@ import React from "react";
 import "./cardComponent.css";
 
 export const CardComponent = ({ product }) => {
-  // Function to render stars based on rating
   const renderStars = (rating) => {
     const stars = [];
     for (let i = 1; i <= 5; i++) {
       if (i <= rating) {
-        stars.push(<span key={i}>&#9733;</span>); // Filled star
+        stars.push(<span key={i}>&#9733;</span>); 
       } else {
-        stars.push(<span key={i}>&#9734;</span>); // Empty star
+        stars.push(<span key={i}>&#9734;</span>); 
       }
     }
     return stars;
   };
 
   return (
-    <div className="container">
+    <div className="product-card">
       <div className="img">
         <img src={product.imgURL} alt="Product Image" />
       </div>
       <div className="data" dir="rtl">
         <div className="price">
           <div >
-            <span className="discounted-price">{product.discountedPrice}₪</span>
+            <span className="discounted-price">₪{product.discountedPrice}</span>
             <span className="original-price">{product.originalPrice}</span>
           </div>
 
-          <div className="rating" dir="ltr">{renderStars(product.rating)}</div>
+          <div className="star-rating" dir="ltr">{renderStars(product.rating)}</div>
         </div>
 
         <p className="description">{product.description}</p>
