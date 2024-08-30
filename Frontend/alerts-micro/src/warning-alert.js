8619fa1d8f3212839cdd40fdb14be8b5c2ce861e
Mohammad <s12010803@stu.najah.edu>
2024-08-30 14:50:30 +0300

    Add warning alerts component

--- Frontend/alerts-micro/src/warning-alert.js
+++ Frontend/alerts-micro/src/warning-alert.js
@@ -2,13 +2,51 @@ import { LitElement, html, css } from 'lit';
 
 export class WarningAlert extends LitElement {
   static styles = css`
-    h1 {
-      color: red;
+    :host {
+      border: 1px solid red;
+      background-color: #faeaeb;
+      border-color: #edc7c7;
+      color: #ca090d;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+      font-size: 13px;
+    }
+    .button {
+      background-color: #3a5795;
+      color: white;
+      padding: 5px 10px;
+      margin-left: 10px;
+      border-radius: 5px;
+    }
+    .text-content {
+      flex-grow: 1;
+    }
+    .title {
+      font-weight: bold;
+      margin-right: 10px;
     }
   `;
 
+  static get properties() {
+    return {
+      buttonText: { type: String },
+      alertText: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.buttonText = 'Login';
+    this.alertText = 'Note that you are currently browsing without logging in. It comes from the offers and to complete the purchase process in the easiest and easiest way'
+  }
+
   render() {
-    return html`<h1>Hello from Lit Component!</h1>`;
+    return html`
+      <b class="title">Very important:</b>
+      <span class="text-content">${this.alertText}!</span>
+      <button class="button">${this.buttonText}</button>
+    `;
   }
 }
 
