54c92f6b649a5c071d0c49deec0cc782483b9324
Abood Jallad <67801795+aboodjallad@users.noreply.github.com>
2024-08-31 22:41:00 +0300

    show top brands component in the container

--- Frontend/products-micro/src/App.js
+++ Frontend/products-micro/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { ContainerProductCardComponent } from './components/containerCard/containerProductCardCopmonent';
+import { CardsComponent } from './components/productsSlider/cardsComponent';
 
 function App() {
   return (
     <div className="App">
+      <CardsComponent/>
       <ContainerProductCardComponent/>
     </div>
     
